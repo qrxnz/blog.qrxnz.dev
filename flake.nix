a7729d284ffc0d4b7949ac73d8083d554de01765
Karol Skoczyk <send@qrxnz.dev>
2025-09-14 17:06:49 +0200

    feat: Initial commit
    - Add Nix flake for reproducible environment
    
    - Configure Hugo with the Hermit theme
    
    - Add initial blog posts and content structure
    
    - Set up basic project files and layouts

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            # Blog things
+            hugo
+            dart-sass
+
+            # Formatters
+            treefmt2
+            alejandra
+
+            # Others
+            go-task
+            watchexec
+          ];
+        };
+      }
+    )
+    // {
+      overlays.default = self: pkgs: {
+        hello = self.packages."${pkgs.system}".hello;
+      };
+    };
+}
